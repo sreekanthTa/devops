f3da56a2091f9c29bf88b26dfa14c20c1aa3b437
sreekanthThadigot <sreekanth@ampcome.com>
2024-04-25 22:28:39 +0530

    add delete old logs file

---
+++ deletelogs.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+SOURCE_DIRECTORY="/app"
+
+if [-d $SOURCE_DIRECTORY]
+then
+  echo "Source directory exists"
+else
+  echo "Inv source directory $SOURCE_DIRECTORY"
+  exit 1
+fi
+
+FILES = $(find $SOURCE_DIRECTORY -name "*.log" -mtime +14)
+
+while IFS = read -r line
+do
+  echo "Deleting file"
+  rm -f $line
+
+  echo "File deleted"
+
+
+done <<< $FILES
