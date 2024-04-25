848b5f95e760f689fb8300139397d9a89c558f69
sreekanthThadigot <sreekanth@ampcome.com>
2024-04-25 22:48:15 +0530

    fixes

--- diskusage.sh
+++ diskusage.sh
@@ -10,6 +10,9 @@ do
  USAGE=$(echo line | awk -F " " '{print $6F}' | cut -d "%" -f1)
  FOLDER=$(echo line | awk -F " " '{print $NF}')
 
+ echo "usages is $USAGE"
+ echo "FOLDER IS $FOLDER"
+ 
  if [ $USAGE -ge $DISK_THRESHOLD ]
  then
   MESSAGE+="$FOLDER is more than $DISK_THRESHOLD, Current usage: $USAGE \n"
