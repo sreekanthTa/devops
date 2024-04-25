1e9e1d599df4d1b2dbecaa486723123dcd04a3aa
sreekanthThadigot <sreekanth@ampcome.com>
2024-04-25 22:50:23 +0530

    fixes

--- diskusage.sh
+++ diskusage.sh
@@ -6,17 +6,16 @@ MESSAGE=""
 
 while IFS= read -r line
 do
+    USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
+    FOLDER=$(echo $line | awk -F " " '{print $NF}')
 
- USAGE=$(echo line | awk -F " " '{print $6F}' | cut -d "%" -f1)
- FOLDER=$(echo line | awk -F " " '{print $NF}')
-
- echo "usages is $USAGE"
- echo "FOLDER IS $FOLDER"
- 
- if [ $USAGE -ge $DISK_THRESHOLD ]
- then
-  MESSAGE+="$FOLDER is more than $DISK_THRESHOLD, Current usage: $USAGE \n"
- fi
+    echo "usages is $USAGE"
+    echo "FOLDER IS $FOLDER"
+    
+    if [ $USAGE -ge $DISK_THRESHOLD ]
+    then
+    MESSAGE+="$FOLDER is more than $DISK_THRESHOLD, Current usage: $USAGE \n"
+    fi
 
 done <<< $DISK_USAGE
 
